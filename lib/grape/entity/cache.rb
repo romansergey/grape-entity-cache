aae66356dc1a6d245b8ccebc3d3a00e873a7afc9
Roman Sergey <roman.sergey@gmail.com>
2015-03-02 22:38:11 +0000

    initial commit

---
+++ lib/grape/entity/cache.rb
@@ -0,0 +1,9 @@
+require "grape/entity/cache/version"
+
+module Grape
+  module Entity
+    module Cache
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/grape/entity/cache/version.rb
@@ -0,0 +1,7 @@
+module Grape
+  module Entity
+    module Cache
+      VERSION = "0.0.1"
+    end
+  end
+end
